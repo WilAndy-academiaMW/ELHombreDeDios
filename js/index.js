c7e8d360b8818c04522fe3a9e94e30027e35a86e
william Montesino <williammontesino09jfr@gmail.com>
2025-01-08 15:06:06 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+//slider header pricipal
+
+const elementos = [
+    {imgSrc: '../articulos/img/ElHombreSabio.jpeg', nombre: 'Nicolas', apellido: 'Tesla', fondo: 'img/rayo.webp'},
+    {imgSrc: '../img/piramide.webp', nombre: 'Nombre2', apellido: 'Apellido2', fondo: 'fondo2.jpg'},
+    {imgSrc: '../img/edad media.webp', nombre: 'Nombre3', apellido: 'Apellido3', fondo: 'fondo3.jpg'}
+];
+
+const estilo = [
+    {colorh1: 'red', sombreh1: '1px 1px 1px rgba(41, 41, 41, .9);', colorh2: 'blue', sombreh2: 'orange', colorboton: 'white', fondoboton: 'blue',link:'historia/historia.html'},
+    {colorh1: 'green', sombreh1: '1px 1px 1px rgba(0, 0, 0, .9);', colorh2: 'yellow', sombreh2: 'purple', colorboton: 'black', fondoboton: 'yellow',link:'historia/historia.html'},
+    {colorh1: 'purple', sombreh1: '1px 1px 1px rgba(255, 255, 255, .9);', colorh2: 'orange', sombreh2: 'black', colorboton: 'blue', fondoboton: 'red',link:'historia/historia.html'}
+];
+
+let slider = 0;
+let style = 0;
+
+function updateContent() {
+    document.querySelector('header').innerHTML = `
+      <div>
+         <img src="${elementos[slider].imgSrc}" alt="Imagen de ${elementos[slider].nombre} ${elementos[slider].apellido}" />
+         <div class='slider'>
+           <h3 style='color:${estilo[style].colorh2};'> ¿Que aprendemos de la Biblia?</h3>
+           <h1 style='color:${estilo[style].colorh1}; teXt-shadow:${estilo[style].sombreh1};'>¡Descubre su sabiduria con nosotros!</h1>
+          <a href="${estilo[style].link}"> 
+          <button  class='botonSlider' style='color:${estilo[style].colorboton}; background:${estilo[style].fondoboton}'>Leer más</button>
+         </a>
+          </div>
+      </div>
+    `;
+
+    // Actualiza los índices de slider y style para la próxima iteración
+   // slider = (slider + 1) % elementos.length;
+    //style = (style + 1) % estilo.length;
+}
+//setInterval(updateContent, 3000);
+updateContent();
+
+
+
+
+  
+
+
+ 
