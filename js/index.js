9a23780aeaf01b04bed788e50c876fb080db538e
william Montesino <williammontesino09jfr@gmail.com>
2025-01-15 22:20:21 -0400

    profigo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 //slider header pricipal
 
 const elementos = [
-    {imgSrc: '../articulos/img/ElHombreSabio.jpeg', nombre: 'Nicolas', apellido: 'Tesla', fondo: 'img/rayo.webp'},
-    {imgSrc: '../img/piramide.webp', nombre: 'Nombre2', apellido: 'Apellido2', fondo: 'fondo2.jpg'},
-    {imgSrc: '../img/edad media.webp', nombre: 'Nombre3', apellido: 'Apellido3', fondo: 'fondo3.jpg'}
+    {imgSrc: '../articulos/img/la sulamita.webp', nombre: 'Nicolas', apellido: 'Tesla', fondo: 'img/rayo.webp'},
+    {imgSrc: '../articulos/img/esther.png', nombre: 'Nombre2', apellido: 'Apellido2', fondo: 'fondo2.jpg'},
+    {imgSrc: '../articulos/img/el rey David.jpg', nombre: 'Nombre3', apellido: 'Apellido3', fondo: 'fondo3.jpg'}
 ];
 
 const estilo = [
-    {colorh1: 'red', sombreh1: '1px 1px 1px rgba(41, 41, 41, .9);', colorh2: 'blue', sombreh2: 'orange', colorboton: 'white', fondoboton: 'blue',link:'historia/historia.html'},
-    {colorh1: 'green', sombreh1: '1px 1px 1px rgba(0, 0, 0, .9);', colorh2: 'yellow', sombreh2: 'purple', colorboton: 'black', fondoboton: 'yellow',link:'historia/historia.html'},
+    {colorh1: 'red', sombreh1: '1px 1px 1px rgba(41, 41, 41, .9);', colorh2: 'blue', sombreh2: 'orange', colorboton: 'white', fondoboton: 'blue',link:'articulos/la sulamita.html'},
+    {colorh1: 'green', sombreh1: '1px 1px 1px rgba(0, 0, 0, .9);', colorh2: 'yellow', sombreh2: 'purple', colorboton: 'black', fondoboton: 'yellow',link:'articulos/la sulamita.html'},
     {colorh1: 'purple', sombreh1: '1px 1px 1px rgba(255, 255, 255, .9);', colorh2: 'orange', sombreh2: 'black', colorboton: 'blue', fondoboton: 'red',link:'historia/historia.html'}
 ];
 
@@ -23,17 +23,17 @@ function updateContent() {
            <h3 style='color:${estilo[style].colorh2};'> ¿Que aprendemos de la Biblia?</h3>
 <h1 style='color:${estilo[style].colorh1}; teXt-shadow:${estilo[style].sombreh1};'>¡Descubre su sabiduria con nosotros!</h1>
           <a href="${estilo[style].link}"> 
-                  <!--    <button  class='botonSlider' style='color:${estilo[style].colorboton}; background:${estilo[style].fondoboton}'>Leer más</button>-->
+                     <button  class='botonSlider' style='color:${estilo[style].colorboton}; background:${estilo[style].fondoboton}'>Leer más</button>
          </a>
           </div>
       </div>
     `;
 
     // Actualiza los índices de slider y style para la próxima iteración
-   // slider = (slider + 1) % elementos.length;
-    //style = (style + 1) % estilo.length;
-}
-//setInterval(updateContent, 3000);
+   //  slider = (slider + 1) % elementos.length;
+   //  style = (style + 1) % estilo.length;
+}// 
+setInterval(updateContent, 3000);
 updateContent();
 
 
